5302b9adf68fc1e50d37d01f2ab1018dc40d89b4
Philip <philip@Elger.SDF.PC>
2020-10-08 18:03:11 +0200

    changed everything

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * This is an easy to understand example for hello world
+ */
+
+
+// std includes
+#include <iostream>
+
+
+
+
+// main programm 
+int main(void)
+{
+    std::cout  << "Hello World from Philip" << std::endl; 
+
+
+    return 0; 
+}
